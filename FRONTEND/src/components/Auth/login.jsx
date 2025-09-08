c380a81a121c1606faac871fad6b6cc112b96e2d
mubiru samuel <mubirusamuel002@gmail.com>
2025-09-08 10:36:59 +0300

    sam

--- FRONTEND/src/components/Auth/login.jsx
+++ FRONTEND/src/components/Auth/login.jsx
@@ -1,44 +1,44 @@
-import React from 'react'
-import styles from './Auth.module.css'
-function Login() {
+import React from 'react';
+import styles from './Auth.module.css';
+
+function Login({ onClose }) {
   return (
-    <div className={styles.authContainer}>
-      <form className={styles.authForm}>
-        <h2>Login</h2>
-        <p>Enter your credentials to get on board</p>
+    <div className={styles.modalOverlay} onClick={onClose}>
+      <div className={styles.authContainer} onClick={(e) => e.stopPropagation()}>
+        <form className={styles.authForm}>
+          <h2>Login</h2>
+          <p>Enter your credentials to get on board</p>
 
-        <div className={styles.formElements}>
-             <label htmlFor="email">Email</label>
-             <input type="email" id='email' />
-        </div>
+          <div className={styles.formElements}>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+          </div>
 
-         <div className={styles.formElements}>
+          <div className={styles.formElements}>
             <label htmlFor="password">Password</label>
-            <input type="password" id='password' />
+            <input type="password" id="password" />
+          </div>
+
+          <div className={styles.rememberMe}>
+            <input type="checkbox" id="checkbox" />
+            <label htmlFor="rememberMe">remember me</label>
+          </div>
+
+          <div>
+            <button type="submit">Login</button>
+          </div>
+
+          <div className={styles.alternative}>
+            <p>Not a member? Create an account</p>
+          </div>
+        </form>
+
+        <div className={styles.authPic}>
+          <img src="https://images.pexels.com/photos/2462015/pexels-photo-2462015.jpeg" alt="bed image" />
         </div>
-
-        <div className={styles.rememberMe}>
-          <input type="checkbox" id='checkbox' />
-          <label htmlFor="rememberMe">remember me</label>
-        </div>   
-
-        <div>
-            <button>
-                Login
-            </button>
-        </div>
-
-        <div className="alternative">
-        <p>Not a member? Create an account</p>
-        </div>
-
-      </form>
-
-      <div className={styles.authPic}>
-        <img src="https://images.pexels.com/photos/2462015/pexels-photo-2462015.jpeg" alt="bed image" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
