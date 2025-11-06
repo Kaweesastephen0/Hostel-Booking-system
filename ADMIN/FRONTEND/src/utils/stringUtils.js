926eea82cc579c454431026e722a33b41d3e8547
Kaweesa Stephen <kaweesastephen0@yahoo.com>
2025-11-06 19:49:23 +0300

    delete

--- ADMIN/FRONTEND/src/utils/stringUtils.js
+++
@@ -1,8 +0,0 @@
-/**
- * Capitalizes the first letter of a string.
- * @param {string} s The string to capitalize.
- * @returns {string} The capitalized string.
- */
-export const capitalize = (s) => {
-  return s && s.charAt(0).toUpperCase() + s.slice(1);
-};
