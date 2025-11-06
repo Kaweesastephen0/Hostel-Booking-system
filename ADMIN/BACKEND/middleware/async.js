2c35f712c07c43e02da463b6ede6822d7c78eb86
Kaweesa Stephen <kaweesastephen0@yahoo.com>
2025-11-06 19:46:35 +0300

    del

--- ADMIN/BACKEND/middleware/async.js
+++
@@ -1,6 +0,0 @@
-// Wrapper to handle async/await errors in routes
-const asyncHandler = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-export default asyncHandler;
