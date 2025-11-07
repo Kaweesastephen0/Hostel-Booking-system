b34d446dceba08a377a1e5a22212a26cd449b50e
Kaweesa Stephen <kaweesastephen0@yahoo.com>
2025-11-07 16:48:49 +0300

    cooors 2

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -60,7 +60,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+// app.options("*", cors(corsOptions));
 
 // Body parser middleware
 app.use(express.json({ limit: '10mb' }));
